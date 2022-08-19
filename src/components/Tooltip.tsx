6f4638f044638b973cc3f9c9e1a3954dd099b311
lee1nna <dlgkssk0209@gmail.com>
2022-08-19 23:52:13 +0900

    feat: add tooltip component

---
+++ src/components/Tooltip.tsx
@@ -0,0 +1,13 @@
+import tooltip from '../assets/image/tooltip.png'
+
+function Tooltip() {
+    return (
+            <div className='tooltip_text' data-tooltip-text="이것은 툴팁입니다. css 만으로도 이렇게 간단한 툴팁을 만들 수 있습니다.">
+                <div className='tooltip_wrap'>
+                    <img className='tooltip_img' src={tooltip} alt=""/>
+                </div>
+            </div>
+    )
+}
+
+export default Tooltip
